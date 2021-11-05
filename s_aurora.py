e535307496806e5cb321298028d240c8b1bd75d3
Alwin <alwin.mao@pitt.edu>
2021-11-05 14:56:40 -0700

    small fix

--- s_aurora.py
+++ s_aurora.py
@@ -9,6 +9,6 @@ if not os.path.isfile(str(i)+'_slice.h5'):
     cc.slice(i,'','')
 if not os.path.isfile(str(i)+'_proj.h5'):
     cc.projection(i,'','')
-
-cpa.plot(int(sys.argv[1]))
+if not os.path.isfile(f'aurora{i:03d}.png'):
+    cpa.plot(int(sys.argv[1]))
     
