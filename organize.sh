b131c5868ec09a21ad2d6f110b3b4b6be632e8fd
Alwin Mao <alwin.mao@gmail.com>
2023-01-17 21:47:48 -0500

    add organize.sh, not tested yet

---
+++ organize.sh
@@ -0,0 +1,22 @@
+mkdir raw_proj
+mkdir raw_rot_proj
+mkdir raw_slice
+
+mkdir proj
+mkdir rot_proj
+mkdir slice
+
+mkdir raw_grid
+mkdir grid
+
+mv *rot_proj.h5 rot_proj/.
+mv *rot_proj.h5.* raw_rot_proj/.
+
+mv *proj.h5 proj/.
+mv *proj.h5.* raw_proj/.
+
+mv *slice.h5 slice/.
+mv *slice.h5.* slice/.
+
+mv *.h5.* raw_grid/.
+mv *.h5 grid/.
