aa5fd385430904961658a47380b378fda67f7ee9
Alwin <alwin.mao@pitt.edu>
2023-01-22 11:34:42 -0800

    parse_cholla_printf.py

---
+++ parse_cholla_printf.py
@@ -0,0 +1,52 @@
+# Parse cholla printf data
+import pickle
+
+
+def parse(filename):
+    with open(filename,'r') as ofile:
+        data = ofile.readlines()
+
+    output = {}
+
+    for line in data:
+        tokens = line.split()
+        
+        if len(tokens) == 0:
+            continue
+
+        if len(tokens) <= 7:
+            continue
+
+        value = float(tokens[7])
+        if tokens[0] == 'n_step:':
+            key = 'timestep'
+        elif tokens[0] == 'Time:':
+            key = tokens[1]
+
+        if key not in output:
+            output[key] = []
+
+        output[key].append(value)
+    return output
+
+
+
+
+
+        with open('filename.pickle', 'rb') as handle:
+                b = pickle.load(handle)
+
+def save(filename,data):
+    if type(data) == str or type(filename) != str:
+        print('Save requires filename first, then data')
+        
+    with open('filename.pickle', 'wb') as handle:
+        pickle.dump(a, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+        
+def load(filename):
+    with open('filename.pickle', 'rb') as handle:
+        data = pickle.load(handle)
+    return data
+            
+    
